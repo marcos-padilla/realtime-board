9a63e230ff8daea1fc5317115f7f7798a89da27e
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-04-26 11:18:07 -0400

    feat(backend): add isFavorite prop to boards

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -58,11 +58,7 @@ export default function BoardList({ orgId, query }: BoardListProps) {
 			<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10'>
 				<NewBoardButton orgId={orgId} />
 				{data?.map((board) => (
-					<BoardCard
-						key={board._id}
-						isFavorite={false}
-						{...board}
-					/>
+					<BoardCard key={board._id} {...board} />
 				))}
 			</div>
 		</div>
