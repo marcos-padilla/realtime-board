d87a700a528327f46717513b982d8c946dfc7685
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-04-25 20:46:02 -0400

    feat(frontend): added  new board button

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -6,6 +6,7 @@ import EmptySearch from './empty-search'
 import { useQuery } from 'convex/react'
 import { api } from '@/convex/_generated/api'
 import BoardCard from './board-card'
+import NewBoardButton from './new-board-button'
 
 interface BoardListProps {
 	orgId: string
@@ -40,6 +41,7 @@ export default function BoardList({ orgId, query }: BoardListProps) {
 				{query.favorites ? 'Favorite boards' : 'Team boards'}
 			</h2>
 			<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10'>
+				<NewBoardButton orgId={orgId} />
 				{data?.map((board) => (
 					<BoardCard
 						key={board._id}
