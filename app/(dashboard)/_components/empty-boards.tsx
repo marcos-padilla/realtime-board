8484136b3b0bc67bd01851bb4b8366978e7afece
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-04-26 12:22:38 -0400

    feat(frontend): Added navigation to board after creation

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -5,11 +5,13 @@ import { api } from '@/convex/_generated/api'
 import { useApiMutation } from '@/hooks/use-api-mutations'
 import { useOrganization } from '@clerk/nextjs'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
 
 export default function EmptyBoards() {
 	const { mutate, pending } = useApiMutation(api.board.create)
 	const { organization } = useOrganization()
+	const router = useRouter()
 	const onClick = async () => {
 		if (!organization) return
 		mutate({
@@ -18,7 +20,7 @@ export default function EmptyBoards() {
 		})
 			.then((id) => {
 				toast.success('Board created successfully')
-				// TODO: Redirect to board/[id]
+				router.push(`/board/${id}`)
 			})
 			.catch(() => toast.error('Failed to create board'))
 	}
--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -4,6 +4,7 @@ import { api } from '@/convex/_generated/api'
 import { useApiMutation } from '@/hooks/use-api-mutations'
 import { cn } from '@/lib/utils'
 import { Plus } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
 
 interface NewBoardButtonProps {
@@ -16,12 +17,12 @@ export default function NewBoardButton({
 	disabled,
 }: NewBoardButtonProps) {
 	const { mutate, pending } = useApiMutation(api.board.create)
-
+	const router = useRouter()
 	const onClick = () => {
 		mutate({ orgId, title: 'Untitled' })
 			.then((id) => {
 				toast.success('Board created')
-				// TODO: Redirect to the new board
+				router.push(`/board/${id}`)
 			})
 			.catch(() => {
 				toast.error('Failed to create board')
