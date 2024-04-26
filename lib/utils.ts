c9341fb1ac41873b5d11c2be3e22843017e9c031
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-04-26 17:04:12 -0400

    feat(frontend): added participants to the room

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs))
+}
+
+const COLORS = [
+	'#FF0000',
+	'#00FF00',
+	'#0000FF',
+	'#FFFF00',
+	'#FF00FF',
+	'#00FFFF',
+	'#FFA500',
+	'#800080',
+]
+
+export function connectionIdToColor(connectionId: number): string {
+	return COLORS[connectionId % COLORS.length]
 }
