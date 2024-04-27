1fdd741a731f76c5e2b1efefd923e024c20f27ab
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-04-27 18:27:46 -0400

    feat(frontend): added canvas state and redo and undo functions

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,20 +1,36 @@
 'use client'
 
+import { CanvasMode, CanvasState } from '@/types'
+import { useState } from 'react'
 import Info from './info'
 import Participants from './participants'
 import Toolbar from './toolbar'
-import { useSelf } from '@/liveblocks.config'
+import { useCanRedo, useCanUndo, useHistory } from '@/liveblocks.config'
 
 interface CanvasProps {
 	boardId: string
 }
 
 export default function Canvas({ boardId }: CanvasProps) {
+	const [canvasState, setCanvasState] = useState<CanvasState>({
+		mode: CanvasMode.None,
+	})
+
+	const history = useHistory()
+	const canUndo = useCanUndo()
+	const canRedo = useCanRedo()
 	return (
 		<main className='size-full relative bg-neutral-100 touch-none'>
 			<Info boardId={boardId} />
 			<Participants />
-			<Toolbar />
+			<Toolbar
+				canvasState={canvasState}
+				setCanvasState={setCanvasState}
+				canRedo={canRedo}
+				canUndo={canUndo}
+				undo={() => history.undo()}
+				redo={() => history.redo()}
+			/>
 		</main>
 	)
 }
--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -10,8 +10,25 @@ import {
 	Type,
 	Undo2,
 } from 'lucide-react'
+import { CanvasState } from '@/types'
 
-export default function Toolbar() {
+interface ToolbarProps {
+	canvasState: CanvasState
+	setCanvasState: (newState: CanvasState) => void
+	undo: () => void
+	redo: () => void
+	canUndo: boolean
+	canRedo: boolean
+}
+
+export default function Toolbar({
+	canvasState,
+	canRedo,
+	canUndo,
+	redo,
+	undo,
+	setCanvasState,
+}: ToolbarProps) {
 	return (
 		<div className='absolute top-[50%] -translate-y-1/2 left-2 flex flex-col gap-y-4'>
 			<div className='bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md'>
@@ -56,14 +73,14 @@ export default function Toolbar() {
 				<ToolButton
 					label='Undo'
 					icon={Undo2}
-					onClick={() => {}}
-					isDisabled={true}
+					onClick={undo}
+					isDisabled={!canUndo}
 				/>
 				<ToolButton
 					label='Redo'
 					icon={Redo2}
-					onClick={() => {}}
-					isDisabled={true}
+					onClick={redo}
+					isDisabled={!canRedo}
 				/>
 			</div>
 		</div>
