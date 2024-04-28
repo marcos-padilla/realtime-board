468a685cc15ac46dfa16ff9d7637a4e6263a81a3
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-04-28 09:54:32 -0400

    Refactor canvas component to include cursor translation

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -67,7 +67,11 @@ export default function Canvas({ boardId }: CanvasProps) {
 				onPointerMove={onPointerMove}
 				onPointerLeave={onPointerLeave}
 			>
-				<g>
+				<g
+					style={{
+						transform: `translate(${camera.x}px, ${camera.y}px)`,
+					}}
+				>
 					<CursorsPresence />
 				</g>
 			</svg>
