f71c58c79ac8077efa52960b993d6591d563d9c4
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-05-03 21:48:44 -0400

    feat(frontend): added send tu back function

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -8,7 +8,7 @@ import ColorPicker from './color-picker'
 import { useDeleteLayers } from '@/hooks/use-delete-layers'
 import Hint from '@/components/globals/hint'
 import { Button } from '@/components/ui/button'
-import { Trash2 } from 'lucide-react'
+import { BringToFront, SendToBack, Trash2 } from 'lucide-react'
 
 interface SelectionToolsProps {
 	camera: Camera
@@ -19,6 +19,26 @@ export const SelectionTools = memo(
 	({ camera, setLastUsedColor }: SelectionToolsProps) => {
 		const selection = useSelf((self) => self.presence.selection)
 		const selectionBounds = useSelectionBounds()
+
+		const moveToBack = useMutation(
+			({ storage }) => {
+				const liveLayerIds = storage.get('layerIds')
+				const indices: number[] = []
+				const arr = liveLayerIds.toArray()
+
+				for (let i = 0; i < arr.length; i++) {
+					if (selection.includes(arr[i])) {
+						indices.push(i)
+					}
+				}
+
+				for (let i = 0; i < indices.length; i++) {
+					liveLayerIds.move(indices[i], i)
+				}
+			},
+			[selection]
+		)
+
 		const setFill = useMutation(
 			({ storage }, fill: Color) => {
 				const liveLyaers = storage.get('layers')
@@ -47,6 +67,24 @@ export const SelectionTools = memo(
 				}}
 			>
 				<ColorPicker onChange={setFill} />
+
+				<div className='flex flex-col gap-y-0 5'>
+					<Hint label='Bring to front'>
+						<Button variant={'board'} size={'icon'}>
+							<BringToFront />
+						</Button>
+					</Hint>
+					<Hint label='Send to back' side='bottom'>
+						<Button
+							variant={'board'}
+							size={'icon'}
+							onClick={moveToBack}
+						>
+							<SendToBack />
+						</Button>
+					</Hint>
+				</div>
+
 				<div className='flex items-center pl-2 border-l border-neutral-200'>
 					<Hint label='Delete'>
 						<Button
