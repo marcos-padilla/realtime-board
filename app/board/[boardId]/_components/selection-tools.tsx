da06ecb574f11818f338a7d1237b52663177c407
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-05-03 21:51:10 -0400

    feat(frontend): added move to front function

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -39,6 +39,28 @@ export const SelectionTools = memo(
 			[selection]
 		)
 
+		const moveToFront = useMutation(
+			({ storage }) => {
+				const liveLayerIds = storage.get('layerIds')
+				const indices: number[] = []
+				const arr = liveLayerIds.toArray()
+
+				for (let i = 0; i < arr.length; i++) {
+					if (selection.includes(arr[i])) {
+						indices.push(i)
+					}
+				}
+
+				for (let i = indices.length - 1; i >= 0; i--) {
+					liveLayerIds.move(
+						indices[i],
+						arr.length - 1 - (indices.length - 1 - i)
+					)
+				}
+			},
+			[selection]
+		)
+
 		const setFill = useMutation(
 			({ storage }, fill: Color) => {
 				const liveLyaers = storage.get('layers')
@@ -70,7 +92,11 @@ export const SelectionTools = memo(
 
 				<div className='flex flex-col gap-y-0 5'>
 					<Hint label='Bring to front'>
-						<Button variant={'board'} size={'icon'}>
+						<Button
+							variant={'board'}
+							size={'icon'}
+							onClick={moveToFront}
+						>
 							<BringToFront />
 						</Button>
 					</Hint>
