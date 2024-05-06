d54d4fd3885293900aa87f1ca2f2f3577c662744
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-05-05 20:28:57 -0400

    feat(frontend): added ellipse component

---
+++ app/board/[boardId]/_components/ellipse.tsx
@@ -0,0 +1,36 @@
+import { colorToHex } from '@/lib/utils'
+import { EllipseLayer } from '@/types'
+
+interface EllipseProps {
+	id: string
+	layer: EllipseLayer
+	onPointerDown: (e: React.PointerEvent, id: string) => void
+	selectionColor?: string
+}
+
+export default function Ellipse({
+	id,
+	layer,
+	onPointerDown,
+	selectionColor,
+}: EllipseProps) {
+	return (
+		<ellipse
+			className='drop-shadow-md'
+			onPointerDown={(e) => onPointerDown(e, id)}
+			style={{
+				transform: `translate(
+                    ${layer.x}px,
+                    ${layer.y}px
+               )`,
+			}}
+			cx={layer.width / 2}
+			cy={layer.height / 2}
+			rx={layer.width / 2}
+			ry={layer.height / 2}
+			fill={layer.fill ? colorToHex(layer.fill) : '#000'}
+			stroke={selectionColor || 'transparent'}
+			strokeWidth={1}
+		/>
+	)
+}
